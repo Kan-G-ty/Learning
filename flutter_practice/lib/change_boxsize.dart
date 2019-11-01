50a86180f5123d75587d26ec2072866b6b8a7da0
TykanN <kang3603a@gmail.com>
2019-11-01 18:04:25 +0900

    My first practice

---
+++ flutter_practice/lib/change_boxsize.dart
@@ -0,0 +1,41 @@
+import 'dart:math';
+import 'package:flutter/material.dart';
+
+class ChangeBoxsize extends StatefulWidget {
+  @override
+  _ChangeBoxsizeState createState() => _ChangeBoxsizeState();
+}
+
+class _ChangeBoxsizeState extends State<ChangeBoxsize> {
+  double _width = 50;
+  double _height = 50;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: _buildAppBar(),
+      body: _buildBody(),
+      floatingActionButton: FloatingActionButton(child: Icon(Icons.refresh),
+          onPressed: () {setState(() {
+            _width = Random().nextInt(200).toDouble();
+            _height = Random().nextInt(200).toDouble();
+          });
+          }),
+    );
+  }
+
+  Widget _buildAppBar() {
+    return AppBar();
+  }
+
+  Widget _buildBody() {
+    return Center(
+        child:
+        AnimatedContainer(
+          decoration: BoxDecoration(color: Colors.green[500]),
+          width: _width,
+          height: _height,
+          duration: Duration(milliseconds: 300),
+        )
+    );
+  }
+}
