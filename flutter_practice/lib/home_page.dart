ceaafadf6b2f54e105b07015160f19d29f5af956
TykanN <kang3603a@gmail.com>
2019-11-03 20:16:12 +0900

    fix home route

--- flutter_practice/lib/home_page.dart
+++ flutter_practice/lib/home_page.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_practice/change_boxsize.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -8,7 +10,26 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Container(
+    return Scaffold(
+      appBar: AppBar(),
+      body: _buildBody(),
     );
   }
+
+ Widget _buildBody() {
+    return Column(
+      children: <Widget>[
+      ListView(
+        children: <Widget>[
+          CupertinoButton(
+            color: Colors.white,
+            padding: EdgeInsets.all(10.0),
+            borderRadius: BorderRadius.circular(2),
+            onPressed: (){Navigator.push(context, MaterialPageRoute(builder: (context) => ChangeBoxsize()));},
+            child: Text('크기 변경 애니메이션')
+          ),
+   ],)
+      ],
+    );
+ }
 }
