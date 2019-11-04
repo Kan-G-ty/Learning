282535d43df216d396d89d885d5dc0b0642fbb65
TykanN <kang3603a@gmail.com>
2019-11-04 19:13:39 +0900

    Fix home_page

--- flutter_practice/lib/home_page.dart
+++ flutter_practice/lib/home_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_practice/change_boxsize.dart';
+import 'package:flutter_practice/my_calc.dart';
 import 'package:flutter_practice/my_form_valid.dart';
 import 'package:flutter_practice/my_tab_controller.dart';
 import 'package:flutter_practice/sliver_appbar.dart';
@@ -14,9 +15,28 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(),
-      body: _buildBody(),
+    return Stack(
+      children: <Widget>[
+        Container(
+          height: double.maxFinite,
+          width: double.maxFinite,
+          child: Image.network(
+            'https://cdn.clien.net/web/api/file/F01/5750413/6fde2f9518adf.jpg',
+          ),
+        ),
+        Scaffold(
+          backgroundColor: Colors.transparent,
+          appBar: AppBar(
+            flexibleSpace: SizedBox(
+              child: Placeholder(),
+            ),
+            title: Text('Home'),
+            backgroundColor: Colors.black.withOpacity(0),
+            elevation: 0,
+          ),
+          body: _buildBody(),
+        ),
+      ],
     );
   }
 
@@ -24,6 +44,7 @@ class _HomePageState extends State<HomePage> {
     return SingleChildScrollView(
       padding: EdgeInsets.all(20),
       child: Container(
+        color: Colors.transparent,
         height: 1000,
         child: Center(
           child: Column(
@@ -34,6 +55,7 @@ class _HomePageState extends State<HomePage> {
               _makebutton('Snackbar 구현', MySnackbar()),
               _makebutton('탭 컨트롤러', MyTabController()),
               _makebutton('Form Validation', MyFormValid()),
+              _makebutton('자체 계산기', MyCalc()),
             ],
           ),
         ),
