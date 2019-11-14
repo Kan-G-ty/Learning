0cd5deb27dfd4bc674b4443dd8d64ae53339ff4d
TykanN <kang3603a@gmail.com>
2019-11-14 13:12:11 +0900

    Add asset img

--- flutter_practice/lib/edit_diary.dart
+++ flutter_practice/lib/edit_diary.dart
@@ -16,8 +16,8 @@ class _RemakeState extends State<Remake> {
   GlobalKey previewContainer = new GlobalKey();
   int originalSize = 800;
 
-  Image _image = Image.network(
-    "https://file-download.jandi-box.com/18119981/2019/11/13/15736252450800c3c8483a5fe1a6e9cdce891ed643020?Expires=1573638893&Signature=PpwQKK9Bq0DDtpG3TCOUITJh1HWGJU8dWNmWjSPCnhflR0D24jHuOznDjH8XZr90OLBegDeIz3I4LhWWAXWjIapiy7EWOdmjpQjhAh84-bNSDUOsmx-JxzK6Kk4oDKS70K-OwYn3V3B7rp18L7kj5M7n~FUSNi-uxXrnAPhvPP0Htnme~UZOhq0gSUxtuXojwMjK59FoKI5J9trme7U4lN~6nZtqGPkDSSqDiTXEapg95p10MJQaNmvNaqBlkbF86TxVcfkRKvaFa5hUH~2zon3m~SRpA07o3ZsITueN~j9naM7AGdFPrNlM8UWubpd8WEjItvmSW0pNic7JgmcBGA__&Key-Pair-Id=APKAJ542KAGWCBA2SXPA",
+  Image _image = Image.asset(
+    "lib/img/diary.png",
   );
   Image _image2;
 
@@ -35,7 +35,7 @@ class _RemakeState extends State<Remake> {
       height: (49 * factor).floor(),
       width: (178 * factor).floor(),
       x: (90 * factor).floor(),
-      y: (310 * factor).floor(),
+      y: (350 * factor).floor(),
     );
     return Scaffold(
       body: Container(
@@ -63,7 +63,7 @@ class _RemakeState extends State<Remake> {
                           ),
                           style: TextStyle(
                             color: Colors.black,
-                            fontSize: 10,
+                            fontSize: 20,
                           ),
                         ),
                       ),
@@ -82,7 +82,7 @@ class _RemakeState extends State<Remake> {
                           ),
                           style: TextStyle(
                             color: Colors.black,
-                            fontSize: 10,
+                            fontSize: 16,
                           ),
                         ),
                       ),
