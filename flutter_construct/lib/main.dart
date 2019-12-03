35e3a7d854d28f2f8e7107395581e37b6bd634a3
TykanN <kang3603a@gmail.com>
2019-12-03 17:26:40 +0900

    practice

--- flutter_construct/lib/main.dart
+++ flutter_construct/lib/main.dart
@@ -1,24 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_construct/src/random_list.dart';
 
-import 'ui/todo_page.dart';
-import 'bloc/todo_provider.dart';
-import 'bloc/todo_bloc.dart';
-import 'repository/api.dart';
-
-void main() => runApp(new MyApp());
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return TodoProvider(
-      todoBloc: TodoBloc(API()),
-      child: MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: 'Flutter Demo',
-        theme: ThemeData(primarySwatch: Colors.blue),
-        home: TodoPage(),
-      ),
-    );
+    return MaterialApp(home: RandomList());
   }
 }
