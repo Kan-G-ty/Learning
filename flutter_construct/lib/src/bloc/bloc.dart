39d604055cceb5348b0de33e1aa50a57d2623d9d
TykanN <kang3603a@gmail.com>
2019-12-04 16:24:25 +0900

    complete bloc_example

--- flutter_construct/lib/src/bloc/bloc.dart
+++ flutter_construct/lib/src/bloc/bloc.dart
@@ -1,7 +1,29 @@
+import 'dart:async';
+
 import 'package:english_words/english_words.dart';
 
 class Bloc {
   Set<WordPair> saved = Set<WordPair>();
+
+  final _savedController = StreamController<Set<WordPair>>.broadcast();
+
+  get savedStream => _savedController.stream;
+
+  get addCurrentSaved => _savedController.sink.add(saved);
+
+  addOrRemoveFromSavedList(WordPair item) {
+    if (saved.contains(item)) {
+      saved.remove(item);
+    } else {
+      saved.add(item);
+    }
+
+    _savedController.sink.add(saved);
+  }
+
+  dispose() {
+    _savedController.close();
+  }
 }
 
 var bloc = Bloc();
--- flutter_construct/lib/src/saved.dart
+++ flutter_construct/lib/src/saved.dart
@@ -4,10 +4,6 @@ import 'package:flutter/material.dart';
 import 'bloc/bloc.dart';
 
 class SavedList extends StatefulWidget {
-  SavedList({@required this.saved});
-
-  final Set<WordPair> saved;
-
   @override
   _SavedListState createState() => _SavedListState();
 }
@@ -15,30 +11,36 @@ class SavedList extends StatefulWidget {
 class _SavedListState extends State<SavedList> {
   @override
   Widget build(BuildContext context) {
-    bloc.saved;
     return Scaffold(appBar: AppBar(title: Text('Saved')), body: _buildBody());
   }
 
   Widget _buildBody() {
-    return ListView.builder(
-      itemCount: widget.saved.length * 2,
-      itemBuilder: (context, index) {
-        if (index.isOdd) return Divider();
+    return StreamBuilder<Set<WordPair>>(
+        stream: bloc.savedStream,
+        builder: (context, snapshot) {
+          var saved = Set<WordPair>();
+          if (snapshot.hasData)
+            saved.addAll(snapshot.data);
+          else
+            bloc.addCurrentSaved;
+          return ListView.builder(
+            itemCount: saved.length * 2,
+            itemBuilder: (context, index) {
+              if (index.isOdd) return Divider();
 
-        var realIndex = index ~/ 2;
+              var realIndex = index ~/ 2;
 
-        return _buildRow(widget.saved.toList()[realIndex]);
-      },
-    );
+              return _buildRow(saved.toList()[realIndex]);
+            },
+          );
+        });
   }
 
   Widget _buildRow(WordPair pair) {
     return ListTile(
         title: Text(pair.asPascalCase),
         onTap: () {
-          setState(() {
-            widget.saved.remove(pair);
-          });
+          bloc.addOrRemoveFromSavedList(pair);
         });
   }
 }
